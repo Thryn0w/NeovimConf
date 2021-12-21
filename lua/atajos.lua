752ff74774e1d496da9f7caa7013c086c196cc7d
Traiko <thrynow@gmail.com>
2021-12-21 13:37:22 -0400

    Modificado: nueva conf al nvimtree

--- lua/atajos.lua
+++ lua/atajos.lua
@@ -6,19 +6,21 @@ map('i', '<c-s>', '<ESC>:w<CR>', opts)
 map('n', '<c-s>', ':w<CR>', opts)
 map('n', 'ss', ':w<CR>', opts)
 
-
 -- Close Neovim/Files
 map('n', 'qq', ':q!<CR>', opts)
 map('n', '<c-q>', ':q<CR>', opts)
 
-
 -- Return normal mode
-map('i', 'jk', '<ESC>', {noremap = true, silent = false})
-map('i', 'kj', '<ESC>', {noremap = true, silent = false})
-
+map('i', 'jk', '<ESC>', opts)
+map('i', 'kj', '<ESC>', opts)
 
 --Nav of files in Telescope
-map('n', '<Space>', ':Telescope find_files<CR>', opts)
+--map('n', '<Space>', ':Telescope find_files<CR>', opts)
 
-
+-- Ejecutar python
 map('n', '<F2>', ':w !python<CR>', opts)
+
+-- NvimTree
+map('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
+map('n', 'r', ':NvimTreeRefresh<CR>', opts)
+map('n', 'n', ':NvimTreeFindFile', opts)
